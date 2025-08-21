ffd939d83b6e3c0115765d25400b0924a85eb8fc
swejasonzhang <swejasonzhang@gmail.com>
2025-08-20 23:45:59 -0400

    Setting up the frontend waitlist

---
+++ frontend/postcss.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
