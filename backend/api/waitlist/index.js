2ec379c84eeb75c042c61eb469529b1916051e72
swejasonzhang <swejasonzhang@gmail.com>
2025-08-23 01:43:11 -0400

    Removed unnecessary file in backend

--- backend/api/waitlist/index.js
+++ backend/api/waitlist/index.js
@@ -12,9 +12,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
+  if (req.method === "OPTIONS") return res.status(200).end();
 
   try {
     if (req.method === "POST") {
