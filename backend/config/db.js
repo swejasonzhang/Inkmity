f9392e3b734da03432b94548028cf44e12987c23
swejasonzhang <swejasonzhang@gmail.com>
2025-08-23 02:18:15 -0400

    Fixed small typo with db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   if (mongoose.connections[0].readyState) return;
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
