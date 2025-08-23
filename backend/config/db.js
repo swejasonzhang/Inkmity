d177f6ba13f6f44de4b9df1fe953c1a0acf8f66b
swejasonzhang <swejasonzhang@gmail.com>
2025-08-23 02:09:37 -0400

    Fixing backend errors

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  if (mongoose.connections[0].readyState) return;
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected");
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
   } catch (err) {
-    console.error("❌ MongoDB Connection Failed:", err.message);
-    process.exit(1);
+    console.error(err);
+    throw new Error("MongoDB connection failed");
   }
 };
 
