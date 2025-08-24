cca4c8893820c3edb5144a810fe289a1d1a3fb0e
swejasonzhang <swejasonzhang@gmail.com>
2025-08-24 00:47:52 -0400

    Still fixing

--- Waitlist/frontend/src/pages/WaitlistPage.jsx
+++ Waitlist/frontend/src/pages/WaitlistPage.jsx
@@ -1,21 +1,30 @@
+import { useEffect, useRef } from "react";
 import WaitlistForm from "../components/WaitlistForm";
 
 export default function WaitlistPage() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play().catch((err) => {
+        console.log("Autoplay blocked:", err);
+      });
+    }
+  }, []);
+
   return (
     <div className="relative min-h-screen flex flex-col justify-center">
       <video
+        ref={videoRef}
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        src="/Video.mp4"
         autoPlay
         loop
         muted
+        defaultMuted
         playsInline
-      >
-        <source src="/Video.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-
+      />
       <div className="absolute top-0 left-0 w-full h-full bg-black/40 z-0"></div>
-
       <div className="relative z-10 flex justify-center items-center w-full">
         <WaitlistForm />
       </div>
