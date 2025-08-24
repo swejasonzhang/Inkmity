5e4418c663995ddb3c6b4f4f198f0a62b67d4859
swejasonzhang <swejasonzhang@gmail.com>
2025-08-24 00:13:35 -0400

    Fixed hosting

--- Waitlist/frontend/src/pages/WaitlistPage.jsx
+++ Waitlist/frontend/src/pages/WaitlistPage.jsx
@@ -3,6 +3,16 @@ import WaitlistForm from "../components/WaitlistForm";
 export default function WaitlistPage() {
   return (
     <div className="relative min-h-screen flex flex-col justify-center">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        src="Video.mp4" 
+        autoPlay
+        loop
+        muted
+      />
+
+      <div className="absolute top-0 left-0 w-full h-full bg-black/40 z-0"></div>
+
       <div className="relative z-10 flex justify-center items-center w-full">
         <WaitlistForm />
       </div>
