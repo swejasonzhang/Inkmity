c00533ddb889edf27a4fd164499499a5509dbc9b
swejasonzhang <swejasonzhang@gmail.com>
2025-08-26 22:15:52 -0400

    Getting ready to style the sign up page then the login

--- MVP/frontend/src/App.jsx
+++ MVP/frontend/src/App.jsx
@@ -1,18 +1,14 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <div>
-      <nav>
-        <Link to="/signup">Signup</Link> | <Link to="/login">Login</Link>
-      </nav>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate to="/signup" replace />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
