7f9d3a00a3b8a414e69b085e7a113d643c73d32b
swejasonzhang <swejasonzhang@gmail.com>
2025-09-03 23:58:59 -0400

    Working on Clerk errors for login

--- MVP/frontend/src/main.tsx
+++ MVP/frontend/src/main.tsx
@@ -7,7 +7,11 @@ import "./index.css";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key! Did you forget to add it in .env?");
+}
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
