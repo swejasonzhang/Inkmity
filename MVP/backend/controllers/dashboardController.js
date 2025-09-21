b52bc89f29a3bc0aeac755752b8e2892bb130569
swejasonzhang <swejasonzhang@gmail.com>
2025-09-20 22:49:26 -0400

    Fixed backend imports and implementation since deleting artist file from the backend

--- MVP/backend/controllers/dashboardController.js
+++ MVP/backend/controllers/dashboardController.js
@@ -1,16 +1,16 @@
-import Artist from "../models/Artist.js";
 import User from "../models/User.js";
 
 export const getDashboardData = async (req, res) => {
   try {
     const userId = req.auth.userId;
+
     const user = await User.findOne({ clerkId: userId });
     if (!user) return res.status(404).json({ error: "User not found" });
 
-    const featuredArtists = await Artist.find()
+    const featuredArtists = await User.find({ role: "artist" })
       .sort({ rating: -1 })
       .limit(5)
-      .select("_id name location style priceRange rating");
+      .select("_id username location style priceRange rating");
 
     res.status(200).json({ user, featuredArtists });
   } catch (error) {
